8771025835d7962b3b32b5fde0bd53a7b5f7a91c
Luis Ahumada <luis@ahumada.dev>
2022-02-11 19:17:54 -0500

    First Overworld Update (Character, Camera and Scene)
    Concept for 2.5 in Overworld.
    + Player Object, with it collition, sprite, and movement
    + Camera that follows the character smoothly
    + Blank 3d scene

---
+++ Puzzle Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody theRB;
+    public float moveSpeed;
+    private Vector2 moveInput;
+
+    private void Start()
+    {
+        theRB.freezeRotation = true;
+    }
+
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        moveInput.Normalize();
+
+        // Y movement in 3d is vertical, so we are using Y movement in Z, that is depth.
+        theRB.velocity = new Vector3(moveInput.x * moveSpeed, theRB.velocity.y, moveInput.y * moveSpeed);
+
+    }
+
+}
