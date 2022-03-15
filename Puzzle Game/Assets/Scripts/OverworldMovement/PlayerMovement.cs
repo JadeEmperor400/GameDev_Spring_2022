d5927b4257e1e31f81b8971865347f8aee258485
Luis Ahumada <luis@ahumada.dev>
2022-03-15 17:30:26 -0400

    Overworld 0.0.4
    +Joystick works fine
    +Camera now follows character (works better after build the game than in Unity editor)

--- Puzzle Game/Assets/Scripts/OverworldMovement/PlayerMovement.cs
+++ Puzzle Game/Assets/Scripts/OverworldMovement/PlayerMovement.cs
@@ -7,7 +7,9 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     public Transform movePoint;
     public LayerMask whatStopsMovement;
-
+    float horizontalMovement;
+    float verticalMovement;
+    public bool freezePlayer = false;
 
 
     void Start()
@@ -17,31 +19,58 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        //Character
-        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
-
-        if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
+        if (!freezePlayer)
         {
-            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            horizontalMovement = UltimateJoystick.GetHorizontalAxisRaw("Movement") + Input.GetAxisRaw("Horizontal");
+            if (horizontalMovement > 1f)
             {
-                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, whatStopsMovement))
-                {
-                    //Ball
-                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
-                }
-                
+                horizontalMovement = 1f;
             }
-            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            if (horizontalMovement < -1f)
             {
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, whatStopsMovement))
-                {
-                    //Ball
-                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
-                }
+                horizontalMovement = -1f;
             }
-            
-            
+
+            verticalMovement = UltimateJoystick.GetVerticalAxisRaw("Movement") + Input.GetAxisRaw("Vertical");
+            if (verticalMovement > 1f)
+            {
+                verticalMovement = 1f;
+            }
+            if (verticalMovement < -1f)
+            {
+                verticalMovement = -1f;
+            }
+
+
+            //Character
+            transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
+            {
+                if (Mathf.Abs(horizontalMovement) == 1f)
+                {
+                    if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(horizontalMovement, 0f, 0f), .2f, whatStopsMovement))
+                    {
+                        //Ball
+                        movePoint.position += new Vector3(horizontalMovement, 0f, 0f);
+                    }
+
+                }
+                if (Mathf.Abs(verticalMovement) == 1f)
+                {
+                    if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, verticalMovement, 0f), .2f, whatStopsMovement))
+                    {
+                        //Ball
+                        movePoint.position += new Vector3(0f, verticalMovement, 0f);
+                    }
+                }
+
+
+            }
+
+
         }
+        
 
         
     }
--- Puzzle Game/Assets/Scripts/UI/MainMenu.cs
+++ Puzzle Game/Assets/Scripts/UI/MainMenu.cs
@@ -7,6 +7,14 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     public void PlayGame()
     {
+        //Scenes list.
+        //0 is StartMenu. 1 is Overworld Demo
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
     }
 }
