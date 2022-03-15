7dcae94372348a86895d54b319a23567c7e06476
Luis Ahumada <luis@ahumada.dev>
2022-03-15 12:28:01 -0400

    Overworld 0.0.2
    +Movement works
    -Battle and dialog not implemented
    -Still no touch controllers

--- Puzzle Game/Assets/Scripts/OverworldMovement/PlayerMovement.cs
+++ Puzzle Game/Assets/Scripts/OverworldMovement/PlayerMovement.cs
@@ -4,28 +4,41 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody2D theRB;
-    public float moveSpeed;
-    private Vector2 moveInput;
+    public float moveSpeed = 5f;
+    public Transform movePoint;
+    public LayerMask whatStopsMovement;
 
-    private void Start()
+
+    void Start()
     {
-        theRB.freezeRotation = true;
+        movePoint.parent = null;
     }
 
     void Update()
     {
-        moveInput.x = Input.GetAxisRaw("Horizontal");
-        moveInput.y = Input.GetAxisRaw("Vertical");
-        moveInput.Normalize();
+        //Character
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
 
-        // Y movement in 3d is vertical, so we are using Y movement in Z, that is depth.
-        theRB.velocity = new Vector3(moveInput.x * moveSpeed, theRB.velocity.y, moveInput.y * moveSpeed);
+        if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
+        {
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            {
+                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, whatStopsMovement))
+                {
+                    //Ball
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                }
+                
+            }
+            if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, whatStopsMovement))
+            {
+                //Ball
+                movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+            }
+            
+        }
 
-    }
-
-    void FixedUpdate()
-    {
-        theRB.MovePosition(theRB.position + moveInput * moveSpeed * Time.fixedDeltaTime);
+        
     }
 }
+
