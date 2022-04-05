22cdc1e35d36df05b4353b4c2ae8475fbd28fc12
PetersonNormil <petersonnormil@gmail.com>
2022-04-05 08:49:08 -0400

    fixed safe mode error

--- Puzzle Game/Assets/Scripts/BattleScripts/ACTION/BattleAction.cs
+++ Puzzle Game/Assets/Scripts/BattleScripts/ACTION/BattleAction.cs
@@ -2,11 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< HEAD:Puzzle Game/Assets/Puzzle Assets/BattleScripts/ACTION/ActionSO.cs
-public abstract class ActionSO : ScriptableObject
-=======
+
+
 public abstract class BattleAction : ScriptableObject
->>>>>>> BattleSystem:Puzzle Game/Assets/Scripts/BattleScripts/ACTION/BattleAction.cs
 {
     public int power = 100;
 
