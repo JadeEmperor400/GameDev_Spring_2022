ef711f802c1b145ca21fbf0dfb16d64420889281
PetersonNormil <petersonnormil@gmail.com>
2022-04-06 15:49:46 -0400

    volume and options sliders, integrating into main menu
    - also a simple help panel

---
+++ Puzzle Game/Assets/MainMenuOptionsPanel.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MainMenuOptionsPanel : MonoBehaviour
+{
+
+
+    public GameObject optionsPanel;
+    private bool clickedOn = false;
+
+    [SerializeField]
+    private AudioMixer audioMixer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OptionsOff();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void SetMasterVolume(float masterVolume)
+    {
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(masterVolume) * 20);
+    }
+
+    public void OptionsSwitch()
+    {
+        clickedOn = !clickedOn;
+
+        optionsPanel.SetActive(clickedOn);
+    }
+    public void OptionsOn()
+    {
+        optionsPanel.SetActive(true);
+    }
+
+
+    public void OptionsOff()
+    {
+        optionsPanel.SetActive(false);
+    }
+}
