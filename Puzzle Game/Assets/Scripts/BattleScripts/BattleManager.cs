4accf1f6124efb55624ede2c7a24b8fe0155f353
PetersonNormil <petersonnormil@gmail.com>
2022-04-07 12:28:46 -0400

    title screen and boss/battle music

--- Puzzle Game/Assets/Scripts/BattleScripts/BattleManager.cs
+++ Puzzle Game/Assets/Scripts/BattleScripts/BattleManager.cs
@@ -124,21 +124,24 @@ public class BattleManager : MonoBehaviour
     }
 
     public void BeginBattle(List<EnemyStats> SpawnThese) {
-        StartCoroutine(musicMotor.changeState(battleMusicState));
+        //  StartCoroutine(musicMotor.changeState(battleMusicState));
 
+        Debug.Log(enemy.Count + " IS THE ENEMEY COUNT ");
         if (enemy.Count != 0)
         {
+            Debug.Log("enemy count is not zero");
             var enemyType = enemy[0].gameObject.GetComponent<EnemyDialogue>().GetEnemyType();
-
+            Debug.Log(enemyType);
             if(enemyType == EnemyType.BossEnemy)
             {
                 //go to boss music state
-              
+                Debug.Log("bosssss"); 
                 StartCoroutine(musicMotor.changeState(bossBattleMusicState));
             }
             else
             {
-               
+                Debug.Log("normal enemy");
+
                 StartCoroutine(musicMotor.changeState(battleMusicState));
             }
         }
